b9296e7c16aff5fc93743447ff3cfdcc97a0b000
EgorVeselov2308 <egorveselov2308@gmail.com>
2026-09-19 11:45:27 +0300

    Add lab files

---
+++ ConsoleApplicationLab2/ConsoleApplicationLab2/ConsoleApplicationLab2.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+double max( double a, double b) {
+	if (a > b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+int main() {
+
+	printf("x         y         U\n");
+	double x, y, U;
+
+	for (x = 1; x <= 3; x += 1.3) {
+		for (y = 0.3; y <= 0.5; y += 0.1) {
+			if (x / y < 1) {
+				U = max(log10(x + x / y), (cbrt(y) * (4 * x * x + 1)) / (3 * pow(cos(fabs(x - y)), 2)));
+			} else{
+				U = (tan(x * y) + 2.6) / (sqrt(sin(x)));
+			}
+			printf("x = %.2f, y = %.2f, U = %.2f\n", x, y, U);
+		}
+	}
+	return 0;
+}
+
